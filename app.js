5daca1935435c259224996ac152f516250962d93
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-09-18 18:34:27 -0400

    created and started app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+const app = express();
+
+app.get("/",(req,resp)=>{
+    resp.send("welcome");
+})
+
+app.listen(3000,function(){
+    console.log('Listening on port 3000 now...')
+})
