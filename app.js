dd6f3b1f5819d03973149241409f8003f0560d43
Rohan Saigal <rohan4saigal@gmail.com>
2017-09-19 11:49:36 -0400

    got all the way up to including nunjucks

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require( 'express' );
+const nunjucks = require('nunjucks');
 const app = express();
+const volleyball = require('volleyball');
 
-app.get("/",(req,resp)=>{
-    resp.send("welcome");
-})
+const locals = {
+    title: 'An Example',
+    people: [
+        {name: 'Gandalf'},
+        {name: 'Frodo'},
+        {name: 'Hermione'}
+    ]
+};
 
-app.listen(3000,function(){
-    console.log('Listening on port 3000 now...')
-})
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views');
+
+app.use(volleyball);
+
+app.use(function(req, resp, next) {
+    console.log('this is middleware');
+    next();
+});
+
+app.get('/', (req, resp) => {
+    //resp.send('welcome');
+    console.log('hitting home route');
+    //resp.send("waddup Evan")
+    resp.render('index.html', locals);
+});
+
+
+app.use('/mitch', (req, resp, next) => {
+   resp.send('mitch is dope dope dope');
+});
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000 now...');
+});
