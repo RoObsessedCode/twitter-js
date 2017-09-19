d68124a81f477b26b6bb511770882b25e0af204e
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-09-19 12:56:14 -0400

    creating routes

--- app.js
+++ app.js
@@ -2,37 +2,30 @@ const express = require( 'express' );
 const nunjucks = require('nunjucks');
 const app = express();
 const volleyball = require('volleyball');
+const routes = require('./routes')
 
-const locals = {
-    title: 'An Example',
-    people: [
-        {name: 'Gandalf'},
-        {name: 'Frodo'},
-        {name: 'Hermione'}
-    ]
-};
+app.use('/',routes);
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views');
 
-app.use(volleyball);
 
-app.use(function(req, resp, next) {
+const people = [{name: "Full"}, {name: 'Stacker'}, {name: "Son"}];
+
+
+app.get('/', (req, res) => {
+    console.log('hitting home route');
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
+app.use(function(req, res, next) {
     console.log('this is middleware');
     next();
 });
 
-app.get('/', (req, resp) => {
-    //resp.send('welcome');
-    console.log('hitting home route');
-    //resp.send("waddup Evan")
-    resp.render('index.html', locals);
-});
-
-
-app.use('/mitch', (req, resp, next) => {
-   resp.send('mitch is dope dope dope');
+app.use('/mitch', (req, res, next) => {
+   res.send('mitch is dope dope dope');
 });
 
 app.listen(3000, function(){
